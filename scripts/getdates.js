cdf4b6e548f7f63030d9951997c79ca9c21eb9af
simeon-obianwa <simeonobianwa@gmail.com>
2026-05-12 21:11:04 +0100

    Home Page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,3 @@
-// scripts/getdates.js
 const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
